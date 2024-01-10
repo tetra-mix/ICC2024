8121b658dca31225d71343a59b0f61b9fa907832
tetra-mix <ryouma1128@outlook.jp>
2024-01-10 15:46:01 +0900

    add: firebase setting

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,12 +4,13 @@ import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBqdbnsK-zmo0rJiIK7byPmerU0meuoPC8",
-    authDomain: "test-project-bdc7f.firebaseapp.com",
-    projectId: "test-project-bdc7f",
-    storageBucket: "test-project-bdc7f.appspot.com",
-    messagingSenderId: "929946583943",
-    appId: "1:929946583943:web:5edbcb76294e5ede5f0922"
+    apiKey: "AIzaSyC5Z6M21J0xTciYad2gd-8wGHirtKT9MCY",
+    authDomain: "businesscontest2024.firebaseapp.com",
+    projectId: "businesscontest2024",
+    storageBucket: "businesscontest2024.appspot.com",
+    messagingSenderId: "306990907319",
+    appId: "1:306990907319:web:edecd31be5c904c2fc3232",
+    measurementId: "G-0HEJQ5B5BZ"
 };
 
 const app = initializeApp(firebaseConfig);
